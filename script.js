8ade0f238170adff5b8a30fff7e1a744d45dbffd
skeerthu2206 <skeerthu220706@gmail.com>
2025-12-27 13:09:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+let currentIndex = 0;
+
+function openLightbox(index) {
+    currentIndex = index;
+    lightbox.style.display = "flex";
+    lightboxImg.src = images[index].src;
+}
+
+function closeLightbox() {
+    lightbox.style.display = "none";
+}
+
+function changeImage(step) {
+    currentIndex += step;
+
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    lightboxImg.src = images[currentIndex].src;
+}
